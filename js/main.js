aad28fa909668203a525083b31c83ecb84fe938a
whwa <wesleyhwa@gmail.com>
2023-11-29 14:02:57 -0800

    add elements

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const wordBank = [
+    { word: "violin", hint: "A musical instrument with strings." },
+    { word: "biology", hint: "The study of life." },
+    { word: "toyota", hint: "A Japanese car brand." },
+    { word: "bicycle", hint: "A form of transportation." },
+    { word: "headphones", hint: "A piece of equipment to listen to something." },
+    { word: "coffee", hint: "A popular caffeinated beverage." },
+    { word: "sunset", hint: "The daily disappearance of the sun." },
+    { word: "sunrise", hint: "The daily appearnce of the sun." },
+    { word: "history", hint: "The study of past events and human civilization." },
+    { word: "camera", hint: "A device to capture pictures and record videos." }
+];
